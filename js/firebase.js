deca61d14c9c10084e24c3accebda11617e08aab
Erickbci <batista.erick07@hotmail.com>
2023-04-11 20:36:06 -0300

    Login Com Firebase Adicionado

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLLJqhxioPvbW8kwdm1EQewjsXzsp_YQ",
+    authDomain: "rick-and-morty-memory-ga-eccac.firebaseapp.com",
+    databaseURL: "https://rick-and-morty-memory-ga-eccac-default-rtdb.firebaseio.com",
+    projectId: "rick-and-morty-memory-ga-eccac",
+    storageBucket: "rick-and-morty-memory-ga-eccac.appspot.com",
+    messagingSenderId: "470515209663",
+    appId: "1:470515209663:web:933e9ca3f011baf2cbfd0a"
+};
+firebase.initializeApp(firebaseConfig);
--- js/login.js
+++ js/login.js
@@ -1,21 +1,115 @@
-const input = document.querySelector('.login-input');
-const button = document.querySelector('.login-button');
-const form = document.querySelector('.login-form');
+//up anterior
+// const input = document.querySelector('.login-input');
+// const button = document.querySelector('.login-button');
+// const form = document.querySelector('.login-form');
 
-const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
-    button.removeAttribute('disabled');
-    return;
-  }
-  button.setAttribute('disabled', '');
-}
+// const validateInput = ({ target }) => {
+//   if (target.value.length > 2) {
+//     button.removeAttribute('disabled');
+//     return;
+//   }
+//   button.setAttribute('disabled', '');
+// }
 
-const handleSubmit = (event) => {
-  event.preventDefault();
+// const handleSubmit = (event) => {
+//   event.preventDefault();
   
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
+//   localStorage.setItem('player', input.value);
+//   window.location = 'pages/game.html';
+// }
+
+// input.addEventListener('input', validateInput);
+// form.addEventListener('submit', handleSubmit);
+
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErrors();
 }
 
-input.addEventListener('input', validateInput);
-form.addEventListener('submit', handleSubmit);
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordError();
+}
+
+function login(){
+  showLoading();
+  firebase.auth().signInWithEmailAndPassword(
+    form.email().value, form.password().value
+  ).then(response => {
+    hideLoading();
+    window.location.href = 'pages/game.html'
+  }).catch(error => {
+    hideLoading();
+    alert(getErrorMessages(error))
+  })
+}
+
+function getErrorMessages(error){
+  if(error.code == 'auth/user-not-found'){
+    return "Usuário nao encontrado";
+  }
+  return error.message;
+}
+
+function register(){
+  window.location.href = 'pages/register.html'
+}
+
+function isEmailValid() {
+  const email = form.email().value
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email)
+}
+
+function toggleEmailErrors() {
+  const email = form.email().value
+  form.emailRequiredError().style.display = email ? 'none' : 'block';
+  form.emailInvalidError().style.display = validateEmail(email) ? 'none' : 'block';
+}
+
+function togglePasswordError() {
+  const password = form.password().value;
+  form.passwordRequiredError().style.display = password ? 'none' : 'block';
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  form.recoverPassword().disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+  const password = form.password().value
+  if (!password) {
+    return false;
+  }
+  return true;
+}
+
+function seePassword() {
+  if (form.password().type === 'password') {
+    form.password().type = 'text';
+    form.seePasswordImage().setAttribute('src', './images/olho.svg');   
+    return;
+  } else {
+    form.password().type = 'password';
+    form.seePasswordImage().setAttribute('src', './images/olho-fechado.svg');
+  } 
+
+}
+
+
+const form = {
+  email: () => document.getElementById('email'),
+  emailInvalidError: () => document.getElementById('email-invalid-error'),
+  emailRequiredError: () => document.getElementById('email-required-error'),
+  loginButton: () => document.getElementById('login-button'),
+  password: () => document.getElementById('password'),
+  passwordRequiredError: () => document.getElementById('password-required-error'),
+  recoverPassword: () => document.getElementById('recover-password-button'),
+  seePasswordImage: () => document.getElementById('see-password-img')
+}
