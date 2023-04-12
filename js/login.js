455546c27ce0c0701fa02a785c41f3df80f1d5d6
Erickbci <batista.erick07@hotmail.com>
2023-04-12 19:22:45 -0300

    Excluindo arquivos mortos

--- js/login.js
+++ js/login.js
@@ -1,26 +1,3 @@
-//up anterior
-// const input = document.querySelector('.login-input');
-// const button = document.querySelector('.login-button');
-// const form = document.querySelector('.login-form');
-
-// const validateInput = ({ target }) => {
-//   if (target.value.length > 2) {
-//     button.removeAttribute('disabled');
-//     return;
-//   }
-//   button.setAttribute('disabled', '');
-// }
-
-// const handleSubmit = (event) => {
-//   event.preventDefault();
-  
-//   localStorage.setItem('player', input.value);
-//   window.location = 'pages/game.html';
-// }
-
-// input.addEventListener('input', validateInput);
-// form.addEventListener('submit', handleSubmit);
-
 function onChangeEmail() {
   toggleButtonsDisable();
   toggleEmailErrors();
