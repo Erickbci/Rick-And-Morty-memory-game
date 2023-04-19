3554c31353bb20cdcec59fadc811424b34da52bc
Erickbci <batista.erick07@hotmail.com>
2023-04-18 21:10:37 -0300

    Firebase Auth - recuperação de senha

--- js/login.js
+++ js/login.js
@@ -25,11 +25,27 @@ function getErrorMessages(error){
   if(error.code == 'auth/user-not-found'){
     return "Usuário nao encontrado";
   }
+  if (error.code == 'auth/wrong-password'){
+    return "Senha inválida"
+  }
   return error.message;
 }
 
 function register(){
   window.location.href = 'pages/register/register.html' 
+}
+
+function recoverPassword() {
+  showLoading();
+  firebase.auth().sendPasswordResetEmail(
+    form.email().value
+    ).then(() => {
+      hideLoading();
+      alert('Email enviado com sucesso')
+    }).catch(error => {
+      hideLoading();
+      alert(getErrorMessages(error));
+    })
 }
 
 function isEmailValid() {
